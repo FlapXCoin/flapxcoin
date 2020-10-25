044091930d20ab4d1f0231d1caececfda431e14a
nerdlabs001 <nerdlabs001@gmail.com>
2020-10-25 10:34:22 -0500

    2.1.0.0 for correction

--- src/version.h
+++ src/version.h
@@ -39,7 +39,7 @@ static const int DATABASE_VERSION = 70501;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 200000;
+static const int PROTOCOL_VERSION = 210000;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
