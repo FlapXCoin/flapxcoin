927d4e08192134c242d58bfffa9a7c551253ca64
flapmin <flapmin@gmail.com>
2023-02-12 15:30:54 -0600

    version bump

--- src/version.h
+++ src/version.h
@@ -18,7 +18,7 @@
 // #define CLIENT_VERSION_MAJOR       2
 // #define CLIENT_VERSION_MINOR       1
 // #define CLIENT_VERSION_REVISION    1
-// #define CLIENT_VERSION_BUILD       1
+// #define CLIENT_VERSION_BUILD       2
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR    //Manditory
